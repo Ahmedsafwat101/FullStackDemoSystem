e4fb2523dd5f89be7741eb63490468f0f926117a
Safwatzzz <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-03-04 14:46:37 +0200

    Initial Commit

---
+++ src/main/java/com/example/fullstackdemosystem/FullStackDemoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackdemosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDemoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackDemoSystemApplication.class, args);
+    }
+
+}
